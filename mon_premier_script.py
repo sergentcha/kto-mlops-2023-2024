ff8a83a93b8852570fbb25feebd633f44114af70
sergentcha <98464854+sergentcha@users.noreply.github.com>
2024-02-05 13:47:15 +0000

    feat: ajout de mon premier script python

---
+++ mon_premier_script.py
@@ -0,0 +1,24 @@
+import unittest
+
+"""
+Count names with more than size_critique letters
+"""
+def count_len_names(prenoms):
+    size_critique=7
+    more_than_seven = 0
+    for prenom in prenoms:
+        if len(prenom) > size_critique:
+            more_than_seven += 1
+            print("Prenom supérieur à",size_critique,": " , prenom)
+        else:
+            print("Prenom inférieur ou égal à", size_critique,": " ,prenom)
+    return more_than_seven
+
+class TestNamesMethod(unittest.TestCase):
+     def test_names(self):
+        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        more_than_seven = count_len_names(prenoms=prenoms)
+        self.assertEqual(more_than_seven, 4)
+
+if __name__ == '__main__':
+    unittest.main()
