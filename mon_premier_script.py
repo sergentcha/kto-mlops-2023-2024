a1a611d88864a0c3b8bbcb24f016cd9ff381e64c
sergentcha <98464854+sergentcha@users.noreply.github.com>
2024-02-12 11:52:17 +0000

    fix : amélioriation de mon premier script .py

--- mon_premier_script.py
+++ mon_premier_script.py
@@ -3,15 +3,16 @@ import unittest
 """
 Count names with more than size_critique letters
 """
-def count_len_names(prenoms):
+def count_len_names(prenoms:list) -> int:
     size_critique=7
     more_than_seven = 0
     for prenom in prenoms:
+        # parcours de la liste et regarde si plus grand que la size_critique, et incrementation du compte si oui
         if len(prenom) > size_critique:
             more_than_seven += 1
-            print("Prenom supérieur à",size_critique,": " , prenom)
+            print("Prenom supérieur à",size_critique,": ",prenom)
         else:
-            print("Prenom inférieur ou égal à", size_critique,": " ,prenom)
+            print("Prenom inférieur ou égal à", size_critique,": ",prenom)
     return more_than_seven
 
 class TestNamesMethod(unittest.TestCase):
